6c4efd12af22e208e07d29e837cb837cb5070417
McUberStein <95429589+McUberStein@users.noreply.github.com>
2022-09-10 14:53:12 +0200

    Update funcs.cpp

--- src/funcs.cpp
+++ src/funcs.cpp
@@ -4,13 +4,15 @@ using namespace element;
 using namespace std;
 
 types::elenium_t utils::stoen(string exp, string strarr[]){
- for(int i = 0; i < info::n_of_elements; i++){
-   if(exp == strarr[i]){
-     int out = i + 1;
-     return (types::elenium_t)out;
-   }
- }
- return types::ERR;
+  if(!flags::CASE)
+    exp[0] = toupper(exp[0]), exp[1] = tolower(exp[1]);
+  for(int i = 0; i < info::n_of_elements; i++){
+    if(exp == strarr[i]){
+      int out = i + 1;
+      return (types::elenium_t)out;
+    }
+  }
+  return types::ERR;
 }
 
 string utils::entos(int num, string strarr[]){
